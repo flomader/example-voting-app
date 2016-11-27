319e6af872adc73741a211c4202b49f2094e101d
Florian Mader <florian.mader@gmail.com>
2016-11-27 22:50:40 +0100

    Revert "emoji everywhere"
    This reverts commit 900e6ab628616bb0286839b50d5085006fc31f02.

--- vote/app.py
+++ vote/app.py
@@ -4,10 +4,9 @@ import os
 import socket
 import random
 import json
-import emoji
 
-option_a = os.getenv('OPTION_A', emoji.emojize(':cat_face: ') + "Cats")
-option_b = os.getenv('OPTION_B', emoji.emojize(':dog_face: ') + "Dogs")
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
